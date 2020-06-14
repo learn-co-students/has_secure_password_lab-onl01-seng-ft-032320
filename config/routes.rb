ca792df886d50cad810c139d4804b3f255e777c8
javante_nelson <javantenelson@gmail.com>
2020-06-13 22:35:16 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+
+  get "/signup" => "users#new"
+  post "/signup" => "users#create"
+  get "/users" => "users#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
