03b6fc01fd5d75495a965aeb111511598bc75a98
ferrisbueller66 <github email address>
2020-06-30 00:51:42 -0400

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        
+        @user = User.create(user_params)
+        if @user.save
+           session[:user_id] = @user.id 
+           redirect_to homepage_path
+        else
+            redirect_to users_path
+        end
+        
+    
+    end
+
+    def homepage
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :password, :password_confirmation)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+      #get "/authors/new", to: "authors#new", as: "new_author"
+      get "/users", to: "users#new"
+      post "/users", to: "users#create"
+      get "/homepage", to: "users#homepage", as: "homepage"
+
+      get "/login", to: "sessions#new"
+      post "/login", to: "sessions#create"
+      post "/logout", to: "sessions#delete"
+      
+    root 'application#show'
 end
